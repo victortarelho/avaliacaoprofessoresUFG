e38832a05ac744c3603b47cd651ca8aa8f1f0e9b
brunorfranco <brunozo_rf@hotmail.com>
2014-06-25 22:31:28 -0300

    Refatoracao
    Refatoracao

--- AvaliacaoProfessores/src/com/ufg/avaliacaoprofessores/business/AvaliacaoBusiness.java
+++ AvaliacaoProfessores/src/com/ufg/avaliacaoprofessores/business/AvaliacaoBusiness.java
@@ -12,23 +12,24 @@ import com.ufg.avaliacaoprofessores.vo.AvaliacaoGeralVO;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 
 /**
  *
  * @author Bruno
  */
 public class AvaliacaoBusiness {
-    
-    public void consomeJson(File arquivo){
 
-       try {
-           Reader reader = new InputStreamReader(AvaliacaoBusiness.class.getResourceAsStream("/exemploAvaliacao.json"), "UTF-8");
-           Gson gson = new GsonBuilder().create();
-           AvaliacaoGeralVO avaliacao = gson.fromJson(reader, AvaliacaoGeralVO.class);
-           System.out.println(avaliacao);
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-       
+    public void consomeJson(File arquivo) throws UnsupportedEncodingException {
+        AvaliacaoGeralVO avaliacao = carregaAvaliacao(arquivo);
+        //fazer reflexao dessas infos pros beans do projeto
+        //itim lindo
+        
+    }
+
+    private AvaliacaoGeralVO carregaAvaliacao(File arquivo) throws UnsupportedEncodingException {
+        Reader reader = new InputStreamReader(AvaliacaoBusiness.class.getResourceAsStream("/exemploAvaliacao.json"), "UTF-8");
+        Gson gson = new GsonBuilder().create();
+        return gson.fromJson(reader, AvaliacaoGeralVO.class);
     }
 }
